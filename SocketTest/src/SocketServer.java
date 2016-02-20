e01c023c63c92cbe77f424b7a924d3e10dbf66a5
5_Boron <5_Boron@WakeRobotics.org>
2016-02-19 21:18:04 -0500

    First Python Server and Client - Fixes to Java Client and Server Boron 2/19/2016

--- SocketTest/src/SocketServer.java
+++ SocketTest/src/SocketServer.java
@@ -1,5 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
@@ -19,10 +21,35 @@ public class SocketServer {
 			Socket s = ss.accept();
 
 			InputStream input = s.getInputStream();
+			InputStreamReader reader = new InputStreamReader(input);
+            BufferedReader br = new BufferedReader(reader);
+            String in = br.readLine();
+            
 			OutputStream output = s.getOutputStream();
-
 			OutputStreamWriter writer = new OutputStreamWriter(output);
-			writer.write("Server Connected\n");
+			
+            if (in.equals("isOnTarget")) {
+            	double foo = Math.random();
+            	if (foo < 0.5) {
+                    System.out.println("yes: " + in);
+                    writer.write("yes\n");
+                } else {
+                    System.out.println("no: " + in);
+                    writer.write("no\n");
+                }
+            } else if (in.equals("whatSpeed")) {
+                	double bar = Math.random();
+                	if (bar < 0.5) {
+                        System.out.println(".75: " +  in);
+                        writer.write(".75\n");
+                    } else {
+                        System.out.println(".25: " + in);
+                        writer.write(".25\n");
+                    }
+            }
+            
+			writer.flush();
+			s.close();
 
 		} catch (UnknownHostException e) {
 			e.printStackTrace();

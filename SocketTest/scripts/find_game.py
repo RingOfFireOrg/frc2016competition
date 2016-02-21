e725b8472e35ddf7a860b8c65981d5507359d27e
bmackie <the.bmackie@gmail.com>
2016-02-20 21:00:56 -0500

    Added ablility to stream live video and track it

--- SocketTest/scripts/find_game.py
+++ SocketTest/scripts/find_game.py
@@ -32,25 +32,26 @@ while True:
 	lower = np.array(lower,dtype = "uint8")
 	mask = cv2.inRange(frame, lower, upper)
  
-	# find contours in the masked image and keep the largest one
-	#(_, cnts, _) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-	#	cv2.CHAIN_APPROX_SIMPLE)
-	#c = max(cnts, key=cv2.contourArea)
+	#find contours in the masked frame and keep the largest one
+	(_, cnts, _) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
 
-	#leftmost = tuple(c[c[:,:,0].argmin()][0])
-	#rightmost = tuple(c[c[:,:,0].argmax()][0])
-	#topmost = tuple(c[c[:,:,1].argmin()][0])
-	#bottommost = tuple(c[c[:,:,1].argmax()][0])
-	#print leftmost
-	#print rightmost
-	#print topmost
-	#print bottommost
-	# approximate the contour
-	#peri = cv2.arcLength(c, True)
-	#approx = cv2.approxPolyDP(c, 0.05 * peri, True)
+	if len(cnts) !=0:
+		c = max(cnts, key=cv2.contourArea)
+
+		leftmost = tuple(c[c[:,:,0].argmin()][0])
+		rightmost = tuple(c[c[:,:,0].argmax()][0])
+		topmost = tuple(c[c[:,:,1].argmin()][0])
+		bottommost = tuple(c[c[:,:,1].argmax()][0])
+		print leftmost
+		print rightmost
+		print topmost
+		print bottommost
+		# approximate the contour
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.05 * peri, True)
  
-	# draw a green bounding box surrounding the red game
-	#cv2.drawContours(resized, [approx], -1, (255, 0, 0), 4)
+		# draw a green bounding box surrounding the red game
+		cv2.drawContours(resized, [approx], -1, (255, 0, 0), 4)
 	cv2.imshow("Frame", frame)
-	cv2.waitKey(0)
-
+	cv2.waitKey(1)

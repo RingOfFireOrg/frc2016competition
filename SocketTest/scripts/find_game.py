40b489ba0da9b05cc1cbcb959e38744ab1b2c984
bmackie <the.bmackie@gmail.com>
2016-03-04 20:37:05 -0500

    Added flask

--- SocketTest/scripts/find_game.py
+++ SocketTest/scripts/find_game.py
@@ -4,6 +4,9 @@ import cv2
 import argparse
 import os
 import imutils
+import random
+from flask import Flask, render_template, Response
+from camera import VideoCamera
 
 # load the games image
 #image = cv2.imread("tower.jpeg")
@@ -58,6 +61,45 @@ while True:
     cv2.imshow("Frame", frame)
     cv2.waitKey(1)
     #  decide if it's a hit or not
+
+    hit = random.random()
+
+    if hit > 0.5:
+        wrfile = open("wrfile.txt", "w")
+        wrfile.write("Testing123\n")
+
+        wrfile.close()
+
+    else:
+        wrfile = open("wrfile.txt", "w")
+        wrfile.write("Testing...\n")
+
+        wrfile.close()
+
+        
+    os.rename('wrfile.txt','rdfile.txt')
+
     #  write out the decision to a file, using a lock
     #  start over in the loop
+
+	#  streams video
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    def gen(camera):
+        while True:
+            frame = camera.get_frame()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+    @app.route('/video_feed')
+    def video_feed():
+        return Response(gen(VideoCamera()),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', debug=True)
 

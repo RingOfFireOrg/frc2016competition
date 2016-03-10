a3f4aae943645be2ca82c1b6d428cf0371e7300b
bmackie <the.bmackie@gmail.com>
2016-03-10 01:53:09 +0000

    Fixed bug where it would only launch on program at a time in script

--- SocketTest/scripts/mystart.sh
+++ SocketTest/scripts/mystart.sh
@@ -3,6 +3,6 @@ export VIRTUALENVWAPPER_PYTHON=/usr/bin/python2.7
 export WORKON_HOME=$HOME/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
 workon cv3
-python ~/Github/frc2016competition/SocketTest/scripts/vam.py
-python ~/Github/frc2016competition/SocketTest/scripts/video_server.py
-python ~/Github/frc2016competition/SocketTest/scripts/decision_maker.py
+python ~/Github/frc2016competition/SocketTest/scripts/vam.py &
+python ~/Github/frc2016competition/SocketTest/scripts/video_server.py &
+python ~/Github/frc2016competition/SocketTest/scripts/decision_maker.py &

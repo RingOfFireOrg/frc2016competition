97491c12b4d1993ba762a432bf1df74942b4e96b
bmackie <the.bmackie@gmail.com>
2016-03-10 01:51:49 +0000

    Changed to proper ports changed start up script for pi paths

--- SocketTest/scripts/mystart.sh
+++ SocketTest/scripts/mystart.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+export VIRTUALENVWAPPER_PYTHON=/usr/bin/python2.7
 export WORKON_HOME=$HOME/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
-workon cv
-python ~/github/frc2016competition/SocketTest/scripts/vam.py
-python ~/github/frc2016competition/SocketTest/scripts/video_server.py
-python ~/github/frc2016competition/SocketTest/scripts/decision_maker.py
+workon cv3
+python ~/Github/frc2016competition/SocketTest/scripts/vam.py
+python ~/Github/frc2016competition/SocketTest/scripts/video_server.py
+python ~/Github/frc2016competition/SocketTest/scripts/decision_maker.py
